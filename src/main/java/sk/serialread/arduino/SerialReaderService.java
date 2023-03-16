37ae1de661545c986545b6cca9e3ce863bd83ccc
Ján Kovalčík <jan.kovalcik@uniqa.at>
2023-03-16 14:00:06 +0100

    initial version

--- src/main/java/sk/serialread/arduino/SerialReaderService.java
+++ src/main/java/sk/serialread/arduino/SerialReaderService.java
@@ -1,30 +1,33 @@
 package sk.serialread.arduino;
 
 import com.fazecast.jSerialComm.SerialPort;
+import com.fazecast.jSerialComm.SerialPortDataListener;
+import com.fazecast.jSerialComm.SerialPortEvent;
+import java.io.UnsupportedEncodingException;
 import javax.swing.JLabel;
 
-public class SerialReaderService {
+public class SerialReaderService implements SerialPortDataListener {
 
+    int baudRate = 9600;
+    int dataBits = 8;
+    int stopBits = SerialPort.ONE_STOP_BIT;
+    int parity = SerialPort.NO_PARITY;
     private SerialPort activePort;
     private JLabel lblL;
     private JLabel lblP;
 
     public void connect(SerialPort port) {
         this.activePort = port;
-        this.activePort.setComPortParameters(9600, 8, 1, 0); // default connection settings for Arduino
-        this.activePort.setComPortTimeouts(SerialPort.TIMEOUT_WRITE_BLOCKING, 0, 0); // block until bytes can be written
+        this.activePort.setComPortParameters(baudRate, dataBits, stopBits, parity); // default connection settings for Arduino
+        this.activePort.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, 1000, 0);
 
         if (this.activePort.openPort()) {
             System.out.println(activePort.getSystemPortName() + ": Port is open :)");
+            this.activePort.addDataListener(this);
         } else {
             System.out.println(activePort.getSystemPortName() + ": Failed to open port :(");
             return;
         }
-    }
-
-    public void readSerialPort() {
-        // TODO: read vales from SerialPort and fill lblL, lblP
-
     }
 
     public SerialPort getActivePort() {
@@ -42,5 +45,29 @@ public class SerialReaderService {
 
     public void setLblP(JLabel lblP) {
         this.lblP = lblP;
+    }
+
+    @Override
+    public int getListeningEvents() {
+        return SerialPort.LISTENING_EVENT_DATA_AVAILABLE;
+    }
+
+    @Override
+    public void serialEvent(SerialPortEvent event) {
+        int size = event.getSerialPort().bytesAvailable();
+        byte[] buffer = new byte[size];
+        int numRead = this.activePort.readBytes(buffer, size);
+        System.out.print("Read " + numRead + " bytes -");
+
+        //Convert bytes to String
+        String S = null;
+        try {
+            S = new String(buffer, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+
+        // TODO: read vales from SerialPort and fill lblL, lblP
+        System.out.println("Received -> " + S);
     }
 }
