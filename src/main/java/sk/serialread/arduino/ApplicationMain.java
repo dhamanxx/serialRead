23c0b236b45ce60870b1a30e85a40709223e1593
Jan Kovalcik <jano.kovalcik@gmail.com>
2023-03-17 22:05:04 +0100

    Optimize jssc lib

--- src/main/java/sk/serialread/arduino/ApplicationMain.java
+++ src/main/java/sk/serialread/arduino/ApplicationMain.java
@@ -1,6 +1,8 @@
 package sk.serialread.arduino;
 
-import com.fazecast.jSerialComm.SerialPort;
+import jssc.SerialPort;
+import jssc.SerialPortList;
+
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.FontFormatException;
@@ -40,13 +42,16 @@ public class ApplicationMain extends JDialog implements ItemListener, ActionList
     SerialReaderService service = new SerialReaderService();
 
     private JComboBox cbPorts;
+    private JComboBox baudRate;
     JButton btn;
-    private SerialPort[] availablePorts;
+    private String[] availablePorts;
+
+    private List<Integer> baudRates = Arrays.asList(9600, 19200, 31250, 57600, 74880, 115200);
 
     JLabel lblL;
     JLabel lblP;
     Font csfont;
-    JTextArea errorLog;
+    //JTextArea errorLog;
 
     public ApplicationMain() {
 
@@ -61,42 +66,48 @@ public class ApplicationMain extends JDialog implements ItemListener, ActionList
         ge.registerFont(csfont);
 
         JFrame frame = new JFrame("Serial Reader...");
+        service.setWindow(frame);
 
         // set frame site
-        frame.setMinimumSize(new Dimension(800, 600));
+        frame.setMinimumSize(new Dimension(900, 600));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        errorLog = new JTextArea(10, 30);
-        service.setErrorLog(errorLog);
-        JScrollPane sp = new JScrollPane(errorLog);
+//        errorLog = new JTextArea(10, 30);
+//        service.setErrorLog(errorLog);
+//        JScrollPane sp = new JScrollPane(errorLog);
 
-        availablePorts = SerialPort.getCommPorts();
+        availablePorts = SerialPortList.getPortNames();
 
-        List<String> sortedList = Arrays.stream(availablePorts).sorted((o1, o2) -> o1.getSystemPortName().compareTo(o2.getSystemPortName()))
-            .map(SerialPort::getSystemPortName).collect(Collectors.toList());
-        addErrorLog("=== Available Ports ===");
+        List<String> sortedList = Arrays.stream(availablePorts).sorted((o1, o2) -> o1.compareTo(o2)).collect(Collectors.toList());
+        System.out.println("=== Available Ports ===");
         for (String port : sortedList) {
-            addErrorLog(port);
+            System.out.println(port);
         }
-        addErrorLog("==================");
+        System.out.println("==================");
 
         // create port selections
-        JLabel l = new JLabel("Select serial port ");
+        JLabel l = new JLabel("Select serial port: ");
         btn = new JButton("Connect");
         btn.addActionListener(this);
+
+        JLabel ll = new JLabel("Select speed: ");
 
         // create checkbox
         this.cbPorts = new JComboBox(sortedList.toArray());
         this.cbPorts.setPreferredSize(new Dimension(100, 25));
         this.cbPorts.addItemListener(this);
 
-        lblL = new JLabel("L  0:45:25", SwingConstants.CENTER);
+        this.baudRate = new JComboBox(baudRates.toArray());
+        this.baudRate.setPreferredSize(new Dimension(100, 25));
+        this.baudRate.setSelectedItem(9600);
+
+        lblL = new JLabel("L  0:45:253", SwingConstants.CENTER);
         lblL.setFont(new Font("DSEG7 Modern", Font.PLAIN, 100));
         service.setLblL(lblL);
         JButton btn1 = new JButton("Copy L to clipboard");
         btn1.addActionListener(this);
 
-        lblP = new JLabel("P  0:41:01", SwingConstants.CENTER);
+        lblP = new JLabel("P  0:41:014", SwingConstants.CENTER);
         lblP.setFont(new Font("DSEG7 Modern", Font.PLAIN, 100));
         service.setLblP(lblP);
         JButton btn2 = new JButton("Copy P to clipboard");
@@ -106,62 +117,59 @@ public class ApplicationMain extends JDialog implements ItemListener, ActionList
         JPanel p = new JPanel();
         p.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
+
         c.fill = GridBagConstraints.HORIZONTAL;
         c.weighty = 1.0;
         c.gridx = 0;
         c.gridy = 0;
-        p.add(l, c);
+        p.add(ll, c);
 
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridx = 1;
         c.gridy = 0;
+        p.add(this.baudRate, c);
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weighty = 1.0;
+        c.gridx = 0;
+        c.gridy = 1;
+        p.add(l, c);
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 1;
+        c.gridy = 1;
         p.add(this.cbPorts, c);
 
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridx = 2;
-        c.gridy = 0;
+        c.gridy = 1;
         p.add(btn, c);
 
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridwidth = 2;
         c.gridx = 0;
-        c.gridy = 1;
+        c.gridy = 2;
         p.add(lblL, c);
 
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridwidth = 1;
         c.gridx = 2;
-        c.gridy = 1;
+        c.gridy = 2;
         p.add(btn1, c);
 
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridwidth = 2;
         c.gridx = 0;
-        c.gridy = 2;
+        c.gridy = 3;
         p.add(lblP, c);
 
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridwidth = 1;
         c.gridx = 2;
-        c.gridy = 2;
+        c.gridy = 3;
         p.add(btn2, c);
 
-        JLabel l2 = new JLabel("log: ");
-        c.gridwidth = 3;
-        c.gridx = 0;
-        c.gridy = 3;
-        p.add(l2, c);
-
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridwidth = 3;
-        c.gridx = 0;
-        c.gridy = 4;
-        p.add(sp, c);
-
         frame.add(p);
-
-        errorLog.setEditable(false);
-        addTo(errorLog);
 
         // display it
         frame.pack();
@@ -173,30 +181,15 @@ public class ApplicationMain extends JDialog implements ItemListener, ActionList
         new ApplicationMain();
     }
 
-    private SerialPort findPortByName(String name) {
-        return Arrays.stream(this.availablePorts)
-            .filter(p -> p.getSystemPortName().equals(name))
-            .findFirst()
-            .orElse(null);
-    }
-
-    public void addErrorLog(String text) {
-        System.out.println(text);
-        errorLog.append(text + "\n");
-        errorLog.setCaretPosition(errorLog.getDocument().getLength());
-    }
-
     @Override
     public void actionPerformed(ActionEvent e) {
         String pName = e.getActionCommand();
         if (pName.equals("Connect")) {
-            addErrorLog(pName + " to " + this.cbPorts.getSelectedItem());
-            service.connect(findPortByName(this.cbPorts.getSelectedItem().toString()));
+            service.connect(this.cbPorts.getSelectedItem().toString(), (Integer) this.baudRate.getSelectedItem());
             btn.setText("Disconnect");
             this.cbPorts.setEnabled(false);
         }
         if (pName.equals("Disconnect")) {
-            addErrorLog(pName + " from " + this.cbPorts.getSelectedItem());
             service.disconnect();
             btn.setText("Connect");
             this.cbPorts.setEnabled(true);
--- src/main/java/sk/serialread/arduino/SerialReaderService.java
+++ src/main/java/sk/serialread/arduino/SerialReaderService.java
@@ -1,48 +1,38 @@
 package sk.serialread.arduino;
 
-import com.fazecast.jSerialComm.SerialPort;
-import com.fazecast.jSerialComm.SerialPortDataListener;
-import com.fazecast.jSerialComm.SerialPortEvent;
+import jssc.SerialPort;
+import jssc.SerialPortEvent;
+import jssc.SerialPortEventListener;
+import jssc.SerialPortException;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import javax.swing.*;
 
-public class SerialReaderService implements SerialPortDataListener {
+import static jssc.SerialPort.MASK_RXCHAR;
 
-    int baudRate = 9600;
-    int dataBits = 8;
-    int stopBits = SerialPort.ONE_STOP_BIT;
-    int parity = SerialPort.NO_PARITY;
+public class SerialReaderService implements SerialPortEventListener {
+
+    int dataBits = SerialPort.DATABITS_8;
+    int stopBits = SerialPort.STOPBITS_1;
+    int parity = SerialPort.PARITY_NONE;
     private SerialPort activePort;
+    private String serialPort;
     private JLabel lblL;
     private JLabel lblP;
-    private JTextArea errorLog;
-    BufferedReader br;
+    //    private JTextArea errorLog;
+    private JFrame window;
+    private StringBuilder message = new StringBuilder();
 
-    public void connect(SerialPort port) {
-        this.activePort = port;
-        this.activePort.setComPortParameters(baudRate, dataBits, stopBits, parity); // default connection settings for Arduino
-        this.activePort.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, 1000, 0);
-
-        if (this.activePort.openPort()) {
-            System.out.println(activePort.getSystemPortName() + ": Port is open :)");
-            InputStream inputStream = port.getInputStream();
-            br = new BufferedReader(new InputStreamReader(inputStream));
-            this.activePort.addDataListener(this);
-        } else {
-            System.out.println(activePort.getSystemPortName() + ": Failed to open port :(");
-            return;
-        }
-    }
-
-    public void addErrorLog(String text) {
-        System.out.println(text);
-        if (errorLog != null) {
-            errorLog.append(text + "\n");
-            errorLog.setCaretPosition(errorLog.getDocument().getLength());
+    public void connect(String port, int baudRate) {
+        this.serialPort = port;
+        try {
+            activePort = new SerialPort(port);
+            activePort.openPort();
+            activePort.setParams(baudRate, dataBits, stopBits, parity); // default connection settings for Arduino
+            activePort.setEventsMask(MASK_RXCHAR);
+            activePort.addEventListener(this);
+        } catch (SerialPortException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
         }
     }
 
@@ -51,8 +41,12 @@ public class SerialReaderService implements SerialPortDataListener {
     }
 
     public void disconnect() {
-        activePort.closePort();
-        System.out.println(activePort.getSystemPortName() + ": Port is closed :)");
+        try {
+            activePort.closePort();
+            System.out.println(serialPort + ": Port is closed :)");
+        } catch (SerialPortException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void setLblL(JLabel lblL) {
@@ -63,27 +57,38 @@ public class SerialReaderService implements SerialPortDataListener {
         this.lblP = lblP;
     }
 
-    @Override
-    public int getListeningEvents() {
-        return SerialPort.LISTENING_EVENT_DATA_AVAILABLE;
+
+    public void setWindow(JFrame window) {
+        this.window = window;
     }
 
     @Override
     public void serialEvent(SerialPortEvent event) {
-        try {
-            String inputLine = br.readLine();
-            addErrorLog("Received -> " + inputLine);
-            String[] split = inputLine.split(",");
-            if (split.length == 2) {
-                lblL.setText("L  " + (split[0].length() > 7 ? split[0].substring(0, 7) : split[0]));
-                lblP.setText("P  " + (split[1].length() > 7 ? split[1].substring(0, 7) : split[1]));
+        if (event.isRXCHAR() && event.getEventValue() > 0) {
+            try {
+                byte buffer[] = activePort.readBytes();
+                for (byte b : buffer) {
+                    if ((b == '\r' || b == '\n') && message.length() > 0) {
+                        String inputLine = message.toString();
+                        System.out.println(inputLine);
+                        String[] split = inputLine.split(",");
+                        if (split.length == 2) {
+                            lblL.setText("L  " + (split[0].length() > 8 ? split[0].substring(0, 8) : split[0]));
+                            lblP.setText("P  " + (split[1].length() > 8 ? split[1].substring(0, 8) : split[1]));
+                            window.revalidate();
+                            window.repaint();
+                        }
+                        message.setLength(0);
+                    } else {
+                        if (b != '\n') {
+                            message.append((char) b);
+                        }
+                    }
+                }
+            } catch (SerialPortException ex) {
+                System.out.println(ex);
+                System.out.println("serialEvent");
             }
-        } catch (Exception ex) {
-            addErrorLog(ex.toString());
         }
-    }
-
-    public void setErrorLog(JTextArea errorLog) {
-        this.errorLog = errorLog;
     }
 }
